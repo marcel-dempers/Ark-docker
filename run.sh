92756e5052447bf310c11d8e2cd0da29157ca2cf
marcel-dempers <aimvec@gmail.com>
2021-03-19 20:35:53 +1100

    add aberration instance

--- run.sh
+++ run.sh
@@ -11,21 +11,11 @@ export TERM=linux
 # Change working directory to /ark to allow relative path
 cd /ark
 
-
 # Creating directory tree && symbolic link
 [ ! -d /ark/log ] && mkdir /ark/log
 [ ! -d /ark/backup ] && mkdir /ark/backup
 [ ! -d /ark/staging ] && mkdir /ark/staging
 
-# #check 1 instance for files
-# if [ ! -d /ark/island  ] || [ ! -f /ark/island/version.txt ];then
-# 	echo "No game files found. Installing..."
-# 	mkdir -p /ark/server/ShooterGame/Saved/SavedArks
-# 	mkdir -p /ark/server/ShooterGame/Content/Mods
-# 	mkdir -p /ark/server/ShooterGame/Binaries/Linux/
-# 	touch /ark/server/ShooterGame/Binaries/Linux/ShooterGameServer
-# 	arkmanager install
-# fi
 
 # check if island instance is installed
 if [ ! -d "/ark/island/ShooterGame/Binaries/Linux" ]; then
@@ -48,6 +38,15 @@ if [ ! -d "/ark/scorchedearth/ShooterGame/Binaries/Linux" ]; then
   arkmanager installmod 731604991 @arkmanager-se
 fi
 
+if [ ! -d "/ark/aberration/ShooterGame/Binaries/Linux" ]; then
+  echo "Installing  files in /ark/aberration..."
+  arkmanager install @arkmanager-abby
+  # mods
+  #classic flyer
+  arkmanager installmod 895711211 @arkmanager-abby
+  #s+
+  arkmanager installmod 731604991 @arkmanager-abby
+fi
 
 if test -f "/etc/arkmanager/instances/arkmanager-island.cfg"; then
   echo "arkmanager-island.cfg exists, starting instance..."
@@ -57,6 +56,11 @@ fi
 if test -f "/etc/arkmanager/instances/arkmanager-se.cfg"; then
   echo "arkmanager-se.cfg exists, starting instance..."
   arkmanager start @arkmanager-se
+fi
+
+if test -f "/etc/arkmanager/instances/arkmanager-abby.cfg"; then
+  echo "arkmanager-abby.cfg exists, starting instance..."
+  arkmanager start @arkmanager-abby
 fi
 
 # Stop server in case of signal INT or TERM
