d6a462d1b120dbaa7810c68f1bddcf88ca55d213
marcel-dempers <aimvec@gmail.com>
2021-07-09 15:28:54 +1000

    improve restart script

--- scripts/restart.ps1
+++ scripts/restart.ps1
@@ -1,15 +1,14 @@
 
-$ENV:REASON="scheduled restart"
+$ENV:REASON="routine server restart"
 
 $ENV:KUBECONFIG="C:\Users\aimve\Downloads\marceldempers-dev-kubeconfig.yaml"
-$delayMinutes = 1
+$delayMinutes = 5
+
 while ($delayMinutes -gt 0)
 {
   if (($delayMinutes -eq 60) -or ($delayMinutes -eq 45) -or ($delayMinutes -eq 30) -or ($delayMinutes -eq 15) -or ($delayMinutes -eq 10) -or ($delayMinutes -eq 5) -or ($delayMinutes -eq 2) -or ($delayMinutes -eq 1))
   {
-    kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager broadcast @arkmanager-island "Server will restart in $delayMinutes min. Reason: $ENV:REASON"
-    kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager broadcast @arkmanager-se "Server will restart in $delayMinutes min. Reason: $ENV:REASON"
-    kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager broadcast @arkmanager-abby "Server will restart in $delayMinutes min. Reason: $ENV:REASON"
+    kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager broadcast @all 'Server will restart in $delayMinutes min. Reason: $ENV:REASON'"
   }
 
   Write-Host "Minutes Remaining: $($delayMinutes)"
@@ -17,19 +16,15 @@ while ($delayMinutes -gt 0)
   $delayMinutes -= 1
 }
 
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager broadcast @all 'Server restarting... Reason: $ENV:REASON'"
 
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager broadcast @arkmanager-island "Server restarting... Reason: $ENV:REASON"
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager broadcast @arkmanager-se "Server restarting... Reason: $ENV:REASON"
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager broadcast @arkmanager-abby "Server restarting... Reason: $ENV:REASON"
 Write-Host "saving world..."
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager saveworld @arkmanager-island
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager saveworld @arkmanager-se
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager saveworld @arkmanager-abby
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager saveworld @all" 
+
 Write-Host "running backup..."
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager backup @arkmanager-island
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager backup @arkmanager-se
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager backup @arkmanager-abby
-Write-Host "running restart..."
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager restart @arkmanager-island
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager restart @arkmanager-se
-kubectl -n arkmanager exec -it arkmanager-0 -- arkmanager restart @arkmanager-abby
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager backup @all" 
+
+Write-Host "stop instances..."
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager restart @all" 
+
+
