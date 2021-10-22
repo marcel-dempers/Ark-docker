b99581be1bf8b7ec019c886a6b98b81de9c3cbd1
marcel-dempers <aimvec@gmail.com>
2021-10-23 09:51:44 +1100

    script updates

--- scripts/shutdown.ps1
+++ scripts/shutdown.ps1
@@ -1,17 +1,17 @@
 $ENV:REASON="updating server"
-
 $ENV:KUBECONFIG="C:\Users\aimve\Downloads\marceldempers-dev-2-kubeconfig.yaml"
 
+. $PSScriptRoot\active_instances.ps1 
 
 $delayMinutes = 5
 
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify @all 'Automated routine server maintenance starting in $delayMinutes minutes'"
+foreach ( $i in $instances ){ kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify @$i 'Automated routine server maintenance starting in $delayMinutes minutes'" }
 
 while ($delayMinutes -gt 0)
 {
   if (($delayMinutes -eq 60) -or ($delayMinutes -eq 45) -or ($delayMinutes -eq 30) -or ($delayMinutes -eq 15) -or ($delayMinutes -eq 10) -or ($delayMinutes -eq 5) -or ($delayMinutes -eq 2) -or ($delayMinutes -eq 1))
   {
-    kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager broadcast @all 'Server will shutdown in $delayMinutes min. Reason: $ENV:REASON'"
+    foreach ( $i in $instances ){ kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager broadcast @$i 'Server will shutdown in $delayMinutes min. Reason: $ENV:REASON'" }
   }
 
   Write-Host "Minutes Remaining: $($delayMinutes)"
@@ -19,17 +19,17 @@ while ($delayMinutes -gt 0)
   $delayMinutes -= 1
 }
 
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify @all 'Automated routine server maintenance. Will be up in 20 minutes'"
+foreach ( $i in $instances ){ kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify @$i 'Automated routine server maintenance. Will be up in 20 minutes'" }
 
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager broadcast @all 'Server shutting down... Reason: $ENV:REASON'"
+foreach ( $i in $instances ){ kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager broadcast @$i 'Server shutting down... Reason: $ENV:REASON'" }
 
 Write-Host "saving world..."
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager saveworld @all" 
+foreach ( $i in $instances ){ kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager saveworld @$i" }
 
 Write-Host "running backup..."
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager backup @all" 
+foreach ( $i in $instances ){ kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager backup @$i" }
 
 Write-Host "stop instances..."
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager stop @all" 
+foreach ( $i in $instances ){ kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager stop @$i" }
 
 
