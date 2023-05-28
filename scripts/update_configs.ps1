7370feec1afe3ecafc7a32c7bb2d0ec8803777f8
marcel-dempers <aimvec@gmail.com>
2023-05-28 10:08:11 +1000

    update breeding intervals for slightly faster mutation process

--- scripts/update_configs.ps1
+++ scripts/update_configs.ps1
@@ -1,10 +1,12 @@
 $ENV:KUBECONFIG="C:\Users\aimve\kube-tools\.kube\marceldempers.yaml"
 
+$timestamp = get-date -f MM-dd-yyyy_HH_mm_ss
+(Get-Content .\docs\kubernetes\arkmanager\configmap.yaml) -replace "#updated at: ", "$& $timestamp" | Set-Content .\docs\kubernetes\arkmanager\configmap.yaml
 kubectl apply -n arkmanager -f .\docs\kubernetes\arkmanager\configmap.yaml
 
 while ($True)
 {
-  kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "cat /conf/GameUserSettings.ini | grep EnableCryopodNerf"
+  kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "cat /conf/Game.ini | grep BabyCuddleIntervalMultiplier"
   Start-Sleep 2
 }
 
