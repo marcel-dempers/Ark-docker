ade43c72358f58ea02cd4acd602fdabba362f22c
marcel-dempers <aimvec@gmail.com>
2023-12-06 08:27:56 +1100

    add option to skip game install for development purpose, bump version

--- asa/entrypoint.sh
+++ asa/entrypoint.sh
@@ -6,11 +6,14 @@ echo "checking install..."
 if [ -d "${ARK_DIR}/ShooterGame/Binaries/Win64/" ]; then
   echo "install exists..."
 else 
-  echo "installation not found, installing ..."
-  
-  mkdir -p ${ARK_DIR}
-  su steam -c "/home/steam/steamcmd/steamcmd.sh +force_install_dir ${ARK_DIR} +login anonymous +app_update 2430930 +quit"
-
+  if [ -z "${DISABLE_INSTALL_ON_BOOT}" ]
+  then
+    echo "installation not found, installing ..."
+    mkdir -p ${ARK_DIR}
+    su steam -c "/home/steam/steamcmd/steamcmd.sh +force_install_dir ${ARK_DIR} +login anonymous +app_update 2430930 +quit"
+  else
+    echo "DISABLE_INSTALL_ON_BOOT set to true, not installing!"
+  fi 
 fi
 
 echo 'passing ownership to steam ...'
