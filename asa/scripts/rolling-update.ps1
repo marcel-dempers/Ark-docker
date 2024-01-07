420f89d6365a6c655fefc882c1bdd1bd7ba5356d
marcel-dempers <aimvec@gmail.com>
2024-01-08 10:59:44 +1100

    some helper scripts

---
+++ asa/scripts/rolling-update.ps1
@@ -0,0 +1,8 @@
+$ENV:KUBECONFIG="C:\Users\aimve\kube-tools\.kube\marceldempers.yaml"
+
+.${PWD}\asa\scripts\backup.ps1
+.${PWD}\asa\scripts\stop.ps1
+.${PWD}\asa\scripts\update.ps1
+kubectl -n arkmanager delete po arkmanager-0
+Sleep 8
+.${PWD}\asa\scripts\logs.ps1
