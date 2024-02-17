15fcf809fd0bcf61a8609ea368c4d060372853c4
marcel-dempers <aimvec@gmail.com>
2024-02-17 21:02:21 +1100

    refactor: remove the need for init containers and simplify config mounts

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,24 @@ else
   fi 
 fi
 
+echo "copying server ini files..."
+
+# Destination directory
+CONFIG_TARGET_DIR="$ARK_DIR/ShooterGame/Saved/Config/WindowsServer/"
+
+# create the target config directory if it doesn't exist
+mkdir -p ${CONFIG_TARGET_DIR}
+
+# Check if .ini files exist in the source directory
+if ls /etc/arkmanager/ini/*.ini 1> /dev/null 2>&1; then
+    # If they do, copy them to the destination directory
+    cp /etc/arkmanager/ini/*.ini ${CONFIG_TARGET_DIR}
+else
+    echo "No .ini files found in /etc/arkmanager/ini"
+fi
+
 echo 'passing ownership to steam ...'
 chown -R steam:steam ${ARK_DIR}
-
 
 su -p -c /home/steam/entrypoint-steam.sh steam 
 
