02e51e58e51e59fa9f99cdae843cc92733d2711a
marcel-dempers <aimvec@gmail.com>
2024-09-15 09:03:59 +1000

    make arkmanager notify command live

--- kubernetes/scripts/rolling-update.ps1
+++ kubernetes/scripts/rolling-update.ps1
@@ -1,7 +1,13 @@
 $ENV:KUBECONFIG="C:\Users\aimve\kube-tools\.kube\marceldempers-v3"
 
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify maintenance"
+
 kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager saveworld @all"
 kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager backup @all"
 kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager stop @all"
 kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager update @island"
+
 kubectl -n arkmanager delete po arkmanager-0
+
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager status @all"
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify running"
