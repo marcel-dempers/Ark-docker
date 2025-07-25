76c653838ed607ed17b20fc65e5f0a7068df964e
marcel-dempers <aimvec@gmail.com>
2025-07-26 09:10:32 +1000

    minor updates

--- kubernetes/scripts/rolling-update.ps1
+++ kubernetes/scripts/rolling-update.ps1
@@ -1,11 +1,12 @@
 $ENV:KUBECONFIG="$HOME\.kube\marceldempers-v3"
 
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify maintenance"
+#kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager notify maintenance"
 
 kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager saveworld @all"
 kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager backup @all"
 kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager stop @all"
-kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager update @island"
+
+kubectl -n arkmanager exec -it arkmanager-0 -- bash -c "arkmanager update @ragnarok"
 
 kubectl -n arkmanager delete po arkmanager-0
 
